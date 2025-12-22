17d064650c959b7e4be4e95706fd88f1dd990549
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-12-22 10:56:11 +0100

    Login screen with button

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import icons from "../constants/icons";
 import images from "../constants/images";
 
 const SignIn = () => {
+  const handleLogin = () => {
+    // Handle Google login logic here
+  };
+
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
@@ -21,6 +26,26 @@ const SignIn = () => {
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
             Lad os finde dit nye hjem!
           </Text>
+
+          <Text className="text-lg font-rubik text-center text-black-200 mt-12">
+            Login på OmBolig med Google konto
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-6"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg ml-2 font-rubik-medium text-black-300">
+                Fortsæt med Google
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
