3802210e49169861a9c3a8d21917b9f22742a29f
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-12-22 10:44:26 +0100

    Sign-in screen design implemented

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import images from "../constants/images";
 
 const SignIn = () => {
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Velkommen til OmBolig
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Lad os finde dit nye hjem!
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
